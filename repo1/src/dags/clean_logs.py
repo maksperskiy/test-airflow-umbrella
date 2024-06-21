fd0856ffa993b1bf0a9d8a88391e9affcd3ec5ba
maksperskiy <maksukay@mail.ru>
2024-06-21 14:37:41 +0200

    Code update

--- repo1/src/dags/clean_logs.py
+++ repo1/src/dags/clean_logs.py
@@ -3,11 +3,23 @@ from datetime import timedelta
 import pendulum
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from src.handlers.log_handler import clean_logs
+# from src.handlers.log_handler import clean_logs
 
-from src.dags import Environment
+# from src.dags import Environment
 
 
+def clean_logs(context: Context) -> Dict[str, Any]:
+    """Parse task context."""
+    task = context["task_instance"]
+    print(context["task_instance"])
+    print(context["task_instance"].__dict__)
+    print(context)
+
+    return {
+        "task_id": task.task_id,
+        "log_url": task.log_url,
+        "timestamp": str(context["execution_date"]),
+    }
 with DAG(
     dag_id="clean_logs",
     description="Clean log for a period",
